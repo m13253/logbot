9b9d32e601ddd11f3b0347479d9140cf6d95b204
Star Brilliant <m13253@hotmail.com>
2012-08-25 15:42:24 +0800

    Fix ident error

--- logbot.py
+++ logbot.py
@@ -34,28 +34,28 @@ logging.info(":: Start logging.")
 
 quiting=False
 while not quiting:
-    if not c.sock:
-        quiting=True
-        time.sleep(10)
-        logging.info(":: Restart logging.")
-        os.execlp("python2", "python2", __file__)
-        break
-    raw=c.recvline(block=True)
-    if raw:
-        line=c.parse(line=raw)
-        if line:
-            try:
+    try:
+        if not c.sock:
+            quiting=True
+            time.sleep(10)
+            logging.info(":: Restart logging.")
+            os.execlp("python2", "python2", __file__)
+            break
+        raw=c.recvline(block=True)
+        if raw:
+            line=c.parse(line=raw)
+            if line:
                 if line["cmd"]=="PRIVMSG" and line["dest"]==NICK and line["msg"]==u"Get out of this channel!": # A small hack
                     logging.info(":: %s asked to leave." % line["nick"])
                     c.quit(u"%s asked to leave." % line["nick"])
                     quiting=True
                 else:
                     logging.info(raw)
-        except Exception as e:
-            logging.info(":: Error: %s" % e)
-        except socket.error as e:
-            logging.info(":: Network error: %s" % e)
-            c.quit("Network error.")
+    except Exception as e:
+        logging.info(":: Error: %s" % e)
+    except socket.error as e:
+        logging.info(":: Network error: %s" % e)
+        c.quit("Network error.")
 logging.info(":: Stop logging.")
 
 # vim: et ft=python sts=4 sw=4 ts=4
